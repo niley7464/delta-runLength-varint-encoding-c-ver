2d20c8d6e19df9e247460babafe058bc3bde9afe
Yelin Jeong <yelin@dhcp-v017-008.mobile.uci.edu>
2019-11-05 14:40:20 -0800

    delta and run length

--- main.cpp
+++ main.cpp
@@ -26,11 +26,11 @@ int main(){
 
     uint32_t* originMatrix = NULL;
     uint32_t* deltaMatrix = NULL;
-    unsigned char* runLengthMatrix = NULL;
+    uint32_t* runLengthMatrix = NULL;
 
-    originMatrix = (uint32_t *)malloc(sizeof(uint32_t) * matrixNumber);
-    deltaMatrix = (uint32_t *)malloc(sizeof(uint32_t) * matrixNumber);
-    runLengthMatrix = (unsigned char*)malloc(sizeof(unsigned char) * matrixNumber * 8);
+    originMatrix = (uint32_t *)malloc(sizeof(uint32_t) * matrixNumber); // to save original matrix
+    deltaMatrix = (uint32_t *)malloc(sizeof(uint32_t) * matrixNumber); // to save the result of delta encoding
+    runLengthMatrix = (uint32_t*)malloc((sizeof(uint32_t) + sizeof(int)) * matrixNumber); // to save the result of run length encoding
 
     fread(originMatrix, sizeof(uint32_t), (size_t)matrixNumber, fp);
     // for(int i = 0 ; i < matrixNumber ; i++){
@@ -45,7 +45,7 @@ int main(){
         last = current;
     } 
 
-    //printf("end delta encoding\n");
+    //printf("end delta encoding.\n");
 
     /* saving delta matrix*/
     FILE *sp = fopen("buffer.bin", "wb");
@@ -54,22 +54,79 @@ int main(){
         return -1;
     }
     fwrite(deltaMatrix,sizeof(uint32_t),(size_t)matrixNumber,sp);
+    fclose(sp);
+
+    //printf("saving delta decoding result.\n");
 
     /* show delta matrix */
     //for(int i = 0 ; i < matrixNumber ; i++) printf("%x\n",deltaMatrix[i]);
 
+    free(deltaMatrix);
+
     /* run length encoding */
     FILE *rp = fopen("buffer.bin", "rb");
+    sp = fopen("length.bin","wb");
     if(rp == NULL){
         printf("rp null pointer");
         return -1;
     }
 
-    fread(runLengthMatrix, sizeof(unsigned char), (size_t)matrixNumber * 8, rp);
+    fread(runLengthMatrix, sizeof(uint32_t), (size_t)matrixNumber, rp);
 
-    //unsigned char last = 0;
-    for(int i = 0 ; i < matrixNumber * 4 ; i++){
-        printf("%02x",runLengthMatrix[i]);
+    uint32_t prev = runLengthMatrix[0];
+    int cnt = 1;
+
+    for(int i = 1 ; i < matrixNumber; i++){
+        if(prev==runLengthMatrix[i]) cnt++;
+        else{
+            fwrite(&prev,sizeof(uint32_t),1,sp);
+            //printf("%x ",prev);
+            fwrite(&cnt,sizeof(int),1,sp);
+            //printf("%d\n",cnt);
+            prev = runLengthMatrix[i];
+            cnt = 1;
+        }
+    }
+    //마지막 처리!!!
+
+    free(runLengthMatrix);
+
+    fclose(rp);
+    fclose(sp);
+
+    //printf("end run length encoding.\n");
+
+    /* run length decoding */
+    rp = fopen("length.bin","rb");
+    sp = fopen("decoded_len.bin","wb");
+    uint32_t repeat;
+    for(int i = 0 ; i < matrixNumber-1 ; i++){ // 나중에 마지막 처리하고 1 다시 더하기!!!!
+        if(fread(&repeat,sizeof(uint32_t),1,rp)==0) return -1;
+        if(fread(&cnt,sizeof(int),1,rp)==0) return -1;
+        while(cnt--){
+            //printf("%x\n",repeat);
+            fwrite(&repeat,sizeof(uint32_t),1,sp);
+        }
     }
 
+    //printf("run length decoding done.\n");
+
+    fclose(rp);
+    fclose(sp);
+
+    rp = fopen("decoded_len.bin","rb");
+    sp = fopen("decoded_delta.bin","wb");
+
+    deltaMatrix = (uint32_t *)malloc(sizeof(uint32_t) * matrixNumber);
+    fread(deltaMatrix,sizeof(uint32_t),(size_t)matrixNumber,rp);
+    last = 0;
+
+    for(int i = 0 ; i < matrixNumber ; i++){
+        uint32_t delta = deltaMatrix[i];
+        deltaMatrix[i] = delta+last;
+        last = deltaMatrix[i];
+        printf("%x\n",deltaMatrix[i]);
+    }
+
+    printf("All done.\n");
 }
